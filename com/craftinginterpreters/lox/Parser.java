a2aaec219d21fdd0d8789a5b104eb88f8e13c996
Ying Liu <yingliu4203@gmail.com>
2021-07-15 22:45:52 -0700

    add expr evaluation

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -31,7 +31,7 @@ class Parser {
     while (match(BANG_EQUAL, EQUAL_EQUAL)) {
       Token operator = previous();
       Expr right = comparison();
-      expr = new Expr.Binary(expr, operator, right)
+      expr = new Expr.Binary(expr, operator, right);
     }
 
     return expr;
